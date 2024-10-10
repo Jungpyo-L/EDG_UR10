1ccc52e70ce791cc15407c52463d84246e8d40d9
Jungpyo Lee <ljp0187@gmail.com>
2024-10-10 15:13:14 -0700

    Finish simple robot control example

--- src/simple_robot_control.py
+++ src/simple_robot_control.py
@@ -2,7 +2,7 @@
 
 # Authors: Jungpyo Lee
 # Create: Oct.08.2024
-# Last update: Oct.08.2024
+# Last update: Oct.10.2024
 # Description: This script is primarily for basic robot control using UR10e robot. 
 # It first moves to initial position (position A), then it move position A (poseA) to B (poseB) and rotate by 45 degrees in y-axis (poseC).
 
@@ -23,9 +23,8 @@ import copy
 from netft_utils.srv import *
 from edg_ur10.srv import *
 
-from helperFunction.FT_callback_helper import FT_CallbackHelp
-from helperFunction.fileSaveHelper import fileSaveHelp
 from helperFunction.rtde_helper import rtdeHelp
+from helperFunction.transformation_matrix import getPoseObj
 
 def main():
 
@@ -37,32 +36,29 @@ def main():
   rospy.init_node('edg_experiment')
 
   # Setup helper functions
-  FT_help = FT_CallbackHelp() # it deals with subscription.
-  rospy.sleep(0.5)
   rtde_help = rtdeHelp(125, speed=0.1 , acc= 0.1)
   rospy.sleep(0.5)
-  file_help = fileSaveHelp()
-  rospy.sleep(0.5)
 
-  # Set the TCP offset and calibration matrix
-  rtde_help.setTCPoffset([0, 0, 0.0, 0, 0, 0])
+  # Set the TCP offset and calibration matrix (ex, suction cup: 0.150, ATI_default: 0.464)
+  # You can set the TCP offset here, but it is recommended to set it in the UR program.
+  # If you set it here, endEffectorPose will be different from the actual pose.
+  # rtde_help.setTCPoffset([0, 0, 0.464, 0, 0, 0])
   rospy.sleep(0.2)
 
   # Set the pose A
-  positionA = [-0.520, .300, 0.20]
-  orientationA = tf.transformations.quaternion_from_euler(np.pi,0,np.pi/2,'sxyz') #static (s) rotating (r)
-  poseA = rtde_help.getPoseObj(positionA, orientationA)
+  positionA = [0.520, -0.200, 0.20]
+  orientationA = tf.transformations.quaternion_from_euler(np.pi,0,-np.pi/2,'sxyz') #static (s) rotating (r)
+  poseA = getPoseObj(positionA, orientationA)
 
   # Set the pose B
-  positionB = [-0.620, .200, 0.30]
-  orientationA = tf.transformations.quaternion_from_euler(np.pi,0,np.pi/2,'sxyz') #static (s) rotating (r)
-  poseB = rtde_help.getPoseObj(positionB, orientationA)
+  positionB = [0.620, -0.100, 0.30]
+  orientationA = tf.transformations.quaternion_from_euler(np.pi,0,-np.pi/2,'sxyz') #static (s) rotating (r)
+  poseB = getPoseObj(positionB, orientationA)
 
   # Set the pose C
-  positionB = [-0.620, .200, 0.30]
-  orientationB  = tf.transformations.quaternion_from_euler(np.pi + 45*deg2rad,0,np.pi/2,'sxyz') #static (s) rotating (r)
-  poseC = rtde_help.getPoseObj(positionB, orientationB)
-  
+  positionB = [0.620, -0.100, 0.30]
+  orientationB  = tf.transformations.quaternion_from_euler(np.pi + 45*deg2rad,0,-np.pi/2,'sxyz') #static (s) rotating (r)
+  poseC = getPoseObj(positionB, orientationB)
 
   # try block so that we can have a keyboard exception
   try:
@@ -70,14 +66,17 @@ def main():
     input("Press <Enter> to go to pose A")
     rtde_help.goToPose(poseA)
     rospy.sleep(1)
+    print("poseA: ", rtde_help.getCurrentPose())
 
     input("Press <Enter> to go to pose B")
     rtde_help.goToPose(poseB)
     rospy.sleep(1)
+    print("poseB: ", rtde_help.getCurrentPose())
 
     input("Press <Enter> to go to pose C")
     rtde_help.goToPose(poseC)
     rospy.sleep(1)
+    print("poseC: ", rtde_help.getCurrentPose())
     
 
     print("============ Python UR_Interface demo complete!")
